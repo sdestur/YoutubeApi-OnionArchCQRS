bed7585598bad984da328a0f9843e2496a642a4b
ayaz <sdestur05@gmail.com>
2023-12-28 17:04:19 +0300

    DbContext olusturuldu

--- Infrastructure/YoutubeApi.Persistence/Configurations/CategoryConfiguration.cs
+++ Infrastructure/YoutubeApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -13,7 +13,47 @@ namespace YoutubeApi.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            throw new NotImplementedException();
+            Category category1 = new()
+            {
+                Id = 1,
+                Name = "Elektrik",
+                Priorty = 1,
+                ParentId= 0,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category category2 = new()
+            {
+                Id = 2,
+                Name = "Moda",
+                Priorty = 2,
+                ParentId = 0,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent1 = new()
+            {
+                Id = 3,
+                Name = "Bilgisayar",
+                Priorty = 1,
+                ParentId = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent2 = new()
+            {
+                Id = 4,
+                Name = "Kadın",
+                Priorty = 1,
+                ParentId = 2,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            builder.HasData(category1, category2,parent1,parent2);  
         }
     }
 }
