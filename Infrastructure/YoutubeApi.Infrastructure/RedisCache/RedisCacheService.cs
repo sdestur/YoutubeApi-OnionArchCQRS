36058b04f6c45551a13f0f32271a08cc736356bb
ayaz <sdestur05@gmail.com>
2024-02-08 02:28:34 +0300

    Redis mekanizmasi kuruldu

---
+++ Infrastructure/YoutubeApi.Infrastructure/RedisCache/RedisCacheService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Application.Interfaces.RedisCache;
+
+namespace YoutubeApi.Infrastructure.RedisCache
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly ConnectionMultiplexer redisConnection;
+        private readonly IDatabase database;
+        private readonly RedisCacheSettings settings;
+
+        public RedisCacheService(IOptions<RedisCacheSettings> options)
+        {
+            settings = options.Value;
+            var opt = ConfigurationOptions.Parse(settings.ConnectionString);
+            redisConnection = ConnectionMultiplexer.Connect(opt);
+            database = redisConnection.GetDatabase();   
+         }
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var value = await database.StringGetAsync(key);
+            if (value.HasValue)
+                return JsonConvert.DeserializeObject<T>(value);
+
+            return default;
+        }
+
+        public async Task<T> SetAsync<T>(string key, T value, DateTime? expirationTime = null)
+        {
+            //DateTime to TimeSpan convert
+            TimeSpan timeUnitExpiration = expirationTime.Value - DateTime.Now;
+            await database.StringSetAsync(key, JsonConvert.SerializeObject(value), timeUnitExpiration);
+        }
+    }
+}
